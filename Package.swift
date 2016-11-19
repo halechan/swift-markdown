c66ec71d49823a666cb7c19477aeb14e834b00ec
Hale Chan <hale@HaledeMacBook-Pro.local>
2016-11-19 20:50:45 +0800

    update package

--- Package.swift
+++ Package.swift
@@ -2,9 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "Markdown",
-    targets: [
-        Target(name: "Demo", dependencies:["Markdown"])
-    ],
     dependencies: [
         .Package(url: "https://github.com/halechan/swift-cmark.git", majorVersion: 0, minor: 26)
     ]
